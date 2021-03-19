d860347b52d4980d2e476c4284730ba27a908bee
daisuke-c <09p061@gmail.com>
2021-03-19 20:36:10 +0900

    edit.phpで修正前の値を表示させる

--- web_app/public_html/edit.php
+++ web_app/public_html/edit.php
@@ -8,6 +8,17 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     if (empty($id)) {
         return;
     }
+
+    //DBのデータを値を削除する準備(:〇〇はプレイスフォルダを表す。パラメータの置き場所)
+    $stmt = $pdo->prepare("SELECT * FROM posts WHERE id = :id");
+    //プレイスフォルダに値を設置する
+    //bindValue ($パラメータID, $バインドする値 [, $PDOデータ型定数] )
+    $stmt->bindValue('id', $id, PDO::PARAM_INT);
+    //実行
+    $stmt->execute();
+
+    //
+    $update = $stmt->fetch(PDO::FETCH_ASSOC);
 }
 
 ?>
@@ -24,10 +35,10 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
   <body>
     <h2>編集ページ</h2>
     <form action='./update.php' method='post'>
-      名前: <input type="text" name="name"><br />
+      名前: <input type="text" name="name" value="<?php echo $update['name']?>"><br />
       投稿内容:<br />
-      <textarea name="text" cols="30" rows="10"></textarea><br />
-      <input type="hidden" name="id" value="<?php echo $id ?>">
+      <textarea name="text" cols="30" rows="10"><?php echo $update['text']?></textarea><br />
+      <input type="hidden" name="id" value="<?php echo $update['id'] ?>">
       <input type="submit" name="send" value="更新">
     </form>
     <button onclick="location.href='index.php'">戻る</button>
